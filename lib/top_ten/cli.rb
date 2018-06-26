d9bd5763803a951776b586bfa0f3b16490446bc1
Alavia <alavia.yahya@gmail.com>
2018-06-26 18:40:19 -0400

    Hardcoded country list

--- lib/top_ten/cli.rb
+++ lib/top_ten/cli.rb
@@ -2,6 +2,15 @@ class TopTen::CLI
 
     def call
         puts "Lonely Planet's Top 10 Countries to Visit in 2018:"
+        list_countries
+    end
+
+    def list_countries
+        puts "1. Chile"
+        puts "2. South Korea"
+        puts "3. Portugal"
+        puts "4. Djibouti"
+        puts "5. New Zealand"
     end
 
 end
